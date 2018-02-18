5a065979609d3c796d804c15b59742b1acf881f8
Panagiotis Manoleas <manpan@ltu.se>
2018-02-18 21:58:45 +0100

    Update __main__.;y

--- __main__.py
+++ __main__.py
@@ -1,21 +1,27 @@
 import PySS
 import os
 
-# Ask the user for the path where the assumed file structure with the data is found.
-directory = input('Where are the data files? :')
 
-# If the user does not provide one, the default is used
-
-def execute():
+def execute(directory):
     PySS.polygonal.main(directory=directory, make_plots=False, export=False)
 
-if not directory:
-    directory = None
-    execute()
 
-# If the given directory does not exist, terminate.
-if directory:
-    if os.path.isdir(directory):
-        execute()
-    else:
-        print('The given directory does not exist.')
+def main():
+    # Ask the user for the path where the assumed file structure with the data is found.
+    directory = input('Where are the data files? :')
+
+    # If the user does not provide one, the default is used
+    if not directory:
+        directory = None
+        execute(directory)
+
+    # If the given directory does not exist, print message and terminate.
+    if directory:
+        if os.path.isdir(directory):
+            execute(directory)
+        else:
+            print('The given directory does not exist.')
+
+
+if __name__ == "__main__":
+    main()
