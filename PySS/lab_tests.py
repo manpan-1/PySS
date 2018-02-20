fee3f44f6533e7707cdb863a59b266993a780d28
Panagiotis Manoleas <manpan@ltu.se>
2018-02-20 22:44:19 +0100

    Update lab_test module. Added functionality on CouponTest class

--- PySS/lab_tests.py
+++ PySS/lab_tests.py
@@ -118,10 +118,73 @@ class CouponTest(Experiment):
         self.thickness = thickness
         self.width = width
         self.l_0 = l_0
+        # If geometric data existe, calculate the initial area.
+        if (
+            isinstance(thickness, float) or isinstance(thickness, int)
+        ) and (
+            isinstance(width, float) or isinstance(width, int)
+        ):
+            self.A_0 = thickness * width
+        else:
+            self.A_0 = None
+
+    def add_initial_data(self, thickness, width, l_0):
+        """
+        Add initial geometrical data of the coupon specimen.
+
+        Assuming that the imported test data provide stroke-load readings from testing, the geometric data of the coupon
+        (thickness, width, initial gauge length) are needed to calculate the stress-strain curves. The method will
+        overwrite pre existing.
+
+        Parameters
+        ----------
+        thickness : float
+            Coupon's measured thickness.
+        width : float
+            Coupon's measured width.
+        l_0 : float
+            Initial gauge length, %l_{0}%
+
+        """
+        self.thickness = thickness
+        self.width = width
+        self.l_0 = l_0
+        self.A_0 = thickness * width
+
+    def calc_stress_strain(self):
+        """
+        Calculate stress-strain curve.
+
+        Calculate the engineering ant the true stress-strain curves for the coupon test. Initial geometric data as well
+        as test data (stroke, load) must be assigned to the object for the method to run. The method assumes that
+        entries with keys 'Stroke' and 'Load' exit in the data dictionary.
+
+        """
+
+        self.data['StressEng'] = 1000 * self.data['Load'] / self.A_0
+        self.data['StrainEng'] = self.data['Epsilon'] / self.l_0
+
+    def plot_stressstrain_eng(self, ax=None):
+        ax = self.plot2d('StrainEng', 'StressEng', ax=ax)
+        plt.xlabel('Strain, $\epsilon_{eng}$ [%]')
+        plt.ylabel('Stress, $\sigma_{eng}$ [MPa]')
+        plt.title(self.name)
+        return ax
 
 
 def main():
     cp = []
     for i in range(1, 7):
-        cp.append(CouponTest.load_data('./data/coupons/cp{}.asc'.format(i)))
+        cp.append(CouponTest.from_file('./data/coupons/cp{}.asc'.format(i)))
+
+    widths = [20.408, 20.386, 20.397, 20.366, 20.35, 20.39]
+    thicknesses = [1.884, 1.891, 1.9, 1.88, 1.882, 1.878]
+    l_0s = [80., 80., 80., 80., 80., 80.]
+
+    for i , x in enumerate(cp):
+        x.add_initial_data(thicknesses[i], widths[i], l_0s[i])
+        x.calc_stress_strain()
+        x.plot_stressstrain_eng()
+
+    return cp
 
