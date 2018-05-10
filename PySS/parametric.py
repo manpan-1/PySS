9ce75394fddfa717d7578a3e2839632aff3a51ca
manpan-1 <manpan@ltu.se>
2018-05-10 15:30:30 +0200

    update parametric.py, better queuing system for parallel jobs

--- PySS/parametric.py
+++ PySS/parametric.py
@@ -3,6 +3,11 @@ from shutil import rmtree
 from time import sleep
 from random import random
 import os
+import filelock_git.filelock as flck
+
+
+def testfunc(*args, **kargs):
+    return("")
 
 def parametric_run(
     prj_name,
@@ -40,7 +45,8 @@ def parametric_run(
         A list of the keys of all the keyword arguments that need to be executed parametrically, similarly to p_args.
     mk_subdirs : bool, optional
         Perform each execution in a separate subdirectory.
-        Useful when the executed job generates files on the working directory.
+        Useful when the executed job generates files on the working directory. If subdirectory with the same name
+        exists, the job will run in the directory ignoring pre-existing files.
         Default is False (run all jobs in the current directory)
     del_subdirs : bool, optional
         Remove the subdirectories for the individual jobs and keep only the results summary.
@@ -88,35 +94,53 @@ def parametric_run(
     all_params = param_args + param_kargs
     combinations = list(product(*all_params))
 
-    # Open a file to collect the results
-    out_file = open('./' + prj_name + '_info.dat', 'a')
+    # Check for existing "last_job.log" and if not, initialise a new one
+    queue = "closed_polygon/last_job.log"
+    if not os.path.isfile(queue):
+        with open(queue, "w+") as f:
+            curr_job_nr = -1
+            f.write(str(curr_job_nr))
+    else:
+        with open(queue, "r") as f:
+            curr_job_nr = int(f.read())
 
     # Initiate a list for the results
     prj_results = []
 
     # Loop through the combinations of the given input values
-    for comb_case in combinations:
+    while curr_job_nr < len(combinations)-1:
+        with flck.FileLock(queue):
+            with open(queue, "r+") as f:
+                curr_job_nr = int(f.read()) + 1
+                f.seek(0)
+                f.truncate()
+                f.write(str(curr_job_nr))
+
+        comb_case = combinations[curr_job_nr]
         # Construct an id string for the current job based on the combination. The string is formatted so that it does
         # contain any illegal characters for filename and abaqus job name usage.
         job_id = str(comb_case).translate(None, ",.&*~!()[]{}|;:\'\"`<>?/\\")
         job_id = job_id.replace(" ", "-")
         job_id = job_id + "-" + prj_name
 
-        # Wait some seconds to avoid multiple initiation of jobs
-        if delay_jobs is True:
-            sleep(round(10 * random(), 2))
-        #TODO: does the following line need to be under the if mk_subdirs condition??
-        # Check if the directory exists
+        # Check if the directory exists. If so, continue to the next job.
         if os.path.isdir("./" + job_id):
             print("Job already exists: A directory with the same name'" + job_id + "' exists in the cwd")
             continue
 
-        if mk_subdirs is True:
-            # Make a new subdirectory for the current session
-            os.mkdir(job_id)
+        # Wait some seconds to avoid multiple initiation of jobs
+        if delay_jobs is True:
+            sleep(round(10 * random(), 2))
 
-            # Change working directory
-            os.chdir('./' + job_id)
+        if mk_subdirs is True:
+            if os.path.isdir(job_id):
+                os.chdir(job_id)
+            else:
+                # Make a new subdirectory for the current session
+                os.mkdir(job_id)
+
+                # Change working directory
+                os.chdir(job_id)
 
         # Assemble current job's input arguments
         if func_args:
@@ -136,7 +160,7 @@ def parametric_run(
             current_func_kargs = {}
 
         # The function to be run for the full factorial parametric is called here
-        print('Running job: ' + job_id)
+        print("Running job nr: " + str(curr_job_nr) + " with name: " + job_id)
 
         try:
             # Execute the current job
@@ -152,7 +176,11 @@ def parametric_run(
             # Write each returned string to the file separated by newlines
             # job_return = str(job_return)
             # TODO: create a standardised output that can be imported by the FEM results class in steel_tools.Column titles, input and output.
-            out_file.write(job_id + ", " + return_string + "\n")
+
+            # Open a file to collect the results
+            with flck.FileLock('./' + prj_name + '_info.dat'):
+                with open('./' + prj_name + '_info.dat', 'a') as out_file:
+                    out_file.write(job_id + ", " + return_string + "\n")
 
             # Add the result of the current job to the return list
             prj_results = prj_results + [job_return]
@@ -167,8 +195,38 @@ def parametric_run(
         if mk_subdirs and del_subdirs is True:
             rmtree(job_id)
 
-    # Close the project results file
-    out_file.close()
-
     # Return a list of results
     return prj_results
+
+def subdir_crawler(
+        exec_func,
+        func_args=None,
+        func_kargs=None,
+        prj_name=None,):
+    """
+    Execute function in all subdirs.
+
+    Parameters
+    ----------
+    :param exec_func:
+    :param out_file:
+    :return:
+    """
+    if func_args is None:
+        func_args = ()
+
+    if func_kargs is None:
+        func_kargs = {}
+
+    if prj_name is None:
+        prj_name = ""
+
+    #TODO: list the directories with an if condition, enter them and executue the function
+    all_dirs = [i for i in os.listdir('.') if os.path.isdir(i)]
+
+    for directory in all_dirs:
+        os.chdir(directory)
+        func_return = exec_func(*func_args, **func_kargs)
+        os.chdir("..")
+        with open('./' + prj_name + '_info.dat', 'a') as out_file:
+            out_file.write(directory + ", " + str(func_return) + "\n")
